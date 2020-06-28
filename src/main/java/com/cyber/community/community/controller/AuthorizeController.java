1c6eb252da796c907878e5c6fa1cf17228df26c0
chjxz <chjxz@126.com>
2020-06-29 02:14:09 +0800

    github author login

---
+++ src/main/java/com/cyber/community/community/controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package com.cyber.community.community.controller;
+
+import com.cyber.community.community.dto.AccessTokenDTO;
+import com.cyber.community.community.dto.GithubUser;
+import com.cyber.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("2838b12c5570454039d2");
+        accessTokenDTO.setClient_secret("9f0ec5921be7b039fa07e005a645262f78fc5922");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        return "index";
+    }
+}
